63be1caec83cdc84e1f4b2048c9078c53824116e
saltplay <187832991+saltplay@users.noreply.github.com>
2025-04-17 10:52:05 +0000

    add

---
+++ dist/auto-open-extensions-settings.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name            Auto Open Extensions Settings
+// @description     自动打开拓展设置
+// @updateURL       https://raw.githubusercontent.com/saltplay/Auto-Open-Extensions-Settings/master/dist/auto-open-extensions-settings.user.js
+// @downloadURL     https://raw.githubusercontent.com/saltplay/Auto-Open-Extensions-Settings/master/dist/auto-open-extensions-settings.user.js
+// @version         1.4
+// @author          saltplay
+// @namespace       https://github.com/SillyTavern/SillyTavern
+// @description     自动打开SillyTavern扩展设置面板
+// @match           *://127.0.0.1:8000/*
+// ==/UserScript==
+
+(function () {
+    // 等待10秒后执行
+    setTimeout(function () {
+        console.log(
+            "10 seconds passed, attempting to open extensions drawer..."
+        );
+
+        // 尝试找到并点击按钮
+        const drawerButton = document.querySelector(
+            "#extensions-settings-button .drawer-toggle"
+        );
+        if (drawerButton) {
+            const event = new MouseEvent("click", {
+                view: window,
+                bubbles: true,
+                cancelable: true,
+            });
+            drawerButton.dispatchEvent(event);
+            console.log("Extensions drawer opened automatically");
+        } else {
+            console.log("Extensions drawer button not found");
+        }
+    }, 10000); // 10秒延迟
+})();
